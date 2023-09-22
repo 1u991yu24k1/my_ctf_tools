f3d49a554f55e0dea537658624f49154de71508f
1u991yu24k1 <mycd9427@gmail.com>
2023-09-22 11:53:05 +0900

    add backup to ctf.sh

--- ctf.sh
+++ ctf.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -eu
-
+# . ctf.sh
 
 WSDIR="/tmp/now"
+if [ -d $WSDIR ]; then
+  BAK="/tmp/ctf_bak_`date '%Y%m%d'`";
+  mv -pr $WSDIR $BAK;
+fi
 mkdir -p $WSDIR
 cp -p /exports/my_ctf_tools/ctf.py /tmp/now/x.py && chmod u+x /tmp/now/x.py
 cd $WSDIR
