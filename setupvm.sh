cefa6fdac88412bff1daeb4490a085959d98e451
1u991yu24k1 <mycd9427@gmail.com>
2024-02-07 01:09:47 +0900

    update setupvm.sh

--- setupvm.sh
+++ setupvm.sh
@@ -1,23 +1,47 @@
 #!/bin/bash -eux
 
 ### RUN THIS SCRIPT WITH ROOT USER!!!!
+#  Stop and disable unused service
+## cups, cups-browsed
+systemctl stop cups && systemctl disable cups
+systemctl stop cups-browsed && systemctl disable cups-browsed
+## default apache2
+systemctl stop apache2 && systemctl disable apache2
+
+## avahi
+systemctl mask avahi-daemon && systemctl stop avahi-daemon && systemctl disable avahi-daemon
+systemctl stop avahi-daemon.socket && systemctl disable avahi-daemon.socket
+
+## wpa_supplicant
+systemctl stop wpa_supplicant && systemctl disable wpa_supplicant
+
+# use japanese repository 
+sed -i".bak" -e 's/\/\/us.archive.ubuntu.com/\/\/ftp.jaist.ac.jp/g' /etc/apt/sources.list
+# disable ipv6 in apt(for performace)
+echo 'Acquire::ForceIPv4 "true";' >> /etc/apt/apt.conf.d/99force-ipv4
+
+# remove libreoffice
+apt remove --purge libreoffice*
+apt remove --purge thunderbird
+apt clean
+apt autoremove
+
+
+apt install aptitude # for conflict avoidance
 dpkg --add-architecture i386
 apt update -y 
 
 ## development tooling
+apt install -y open-vm-tools
 apt install -y vim git flex bison manpages nkf libncurses5 patch expect screen lsof lshw mlocate 
-apt install -y zip unzip rar unrar cpio p7zip-full lzip
-apt install -y gcc g++ clang make automake autogen autoconf cmake ninja
-apt install -y binutils strace ltrace gdb gdbserver elfutils dwarves nasm radare2 rr dwarfdump 
+apt install -y zip unzip rar unrar cpio p7zip-full lzip zstd
+apt install -y gcc g++ clang make automake autogen autoconf cmake ninjai llvm lld libclang-dev
+apt install -y clang-13 llvm-13 llvm-13-dev llvm-13-tools
+apt install -y binutils strace ltrace gdb gdbserver elfutils dwarves nasm rr dwarfdump patchelf
 apt install -y gcc-multilib g++-multilib binutils-multiarch gdb-multiarch  
 apt install -y libc6-dbg libmpc-dev libmpfr-dev libelf-dev libc6:i386 libncurses5:i386 libstdc++6:i386 libc6-dbg:i386
+apt install -y libcapstone-dev libseccomp-dev libssl-dev libelf-dev libmpfr-dev libslirp-dev
 
-## Rust(rustc, cargo)
-apt install -y rustc
-curl https://sh.rustup.rs -sSf | sh 
-cargo install rustfilt
-
-#TODO: add "$HOME/.cargo/bin" to $PATH in .bashrc
 ## scripting 
 apt install -y perl python3 python3-dev ruby ruby-dev php 
 
@@ -30,23 +54,42 @@ apt install -y iproute2 wget curl tcpdump tshark netcat socat tcpick tcpflow ngr
 ## ssl/crypto
 apt install -y aespipe john hashcash openssl
 
+apt install -y libcap-dev libgoogle-perftools-dev libncurses5-dev libsqlite3-dev libtcmalloc-minimal4 graphviz doxygen 
+apt install -y python3-pip python3-tabulate exiftool zipinfo 
+aptitude install libegl-mesa0
+
+pushd ~/
+git clone https://github.com/keyunluo/pkcrack
+cd pkcrack
+mkdir build
+cd build
+cmake ..
+make 
+cp ../bin/{extract,findkey,makekey,pkcrack,zipdecrypt} /usr/local/bin/ 
+
+
 ## server infra 
 apt install -y openssh-server dnsutils
 
 ## emulator/vmm
-apt install -y busybox-static docker.io qemu-user qemu-utils fakeroot u-boot-tools
+apt install -y busybox-static qemu-user-static qemu-utils fakeroot u-boot-tools 
 
 ## my development env setup 
 git config --global user.name "1u991yu24k1"
 git config --global user.email mycd9427@gmail.com
 git config --global core.editor /usr/bin/vim
 
-## non apt tools for pwnable
-apt -y install python3-pip
-python3 -m pip --upgrade pip 
-python3 -m pip install hexdump pwntools z3-solver angr pycryptodome requests
+## install non-apt tools
+python3 -m pip install --upgrade pip 
+python3 -m pip install hexdump pwntools z3-solver angr pycryptodome requests lit wllvm keystone-engine
 gem install one_gadget
 gem install seccomp-tools
+gem install zsteg
+
+# docker/docker-compose
+curl -fsSL https://get.docker.com/ | sh
+curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 ### checksec
 wget https://raw.githubusercontent.com/slimm609/checksec.sh/master/checksec -O /usr/local/bin/checksec && chmod +x /usr/local/bin/checksec
@@ -85,10 +128,18 @@ echo "dir $(pwd)/sysdeps/$(uname -p)" >> ~/.gdbinit
 mkdir -p /exports
 cd /exports 
 
+## Rust(rustc, cargo)
+apt install -y rustc
+curl https://sh.rustup.rs -sSf | sh 
+cargo install rustfilt
+
+
 ## qemu system(i386, x86_64, arm, aarch64, riscv32, riscv64)
-apt build-dep qemu
-QEMU_VERSION="6.2.0"
-QM_VER="62"
+# uncomment deb-src(for apt build-dep)
+sed -i -e 's/^# \(deb-src\)/\1/g' /etc/apt/sources.list
+apt build-dep qemu ninja-build
+QEMU_VERSION="7.0.0"
+QM_VER="70"
 
 wget https://download.qemu.org/qemu-$(QEMU_VERSION).tar.xz
 tar xvJf qemu-$(QEMU_VERSION).tar.xz
@@ -99,42 +150,66 @@ make clean
 
 ### i386
 ./configure --target-list=i386-softmmu && make -j
-mv i386-softmmu/qemu-system-i386 /usr/local/bin/qemu"$QM_VER"-system-i386
+mv build/qemu-system-i386 /usr/local/bin/qemu"$QM_VER"-system-i386
 ln -s /usr/local/bin/qemu"$QM_VER"-system-i386 /usr/local/bin/qemu-system-i386
 make clean
 
 ### x64
 ./configure --target-list=x86_64-softmmu && make -j
-mv x86_64-softmmu/qemu-system-x86_64 /usr/local/bin/qemu"$QM_VER"-system-x64
+mv build/qemu-system-x86_64 /usr/local/bin/qemu"$QM_VER"-system-x86_64
 ln -s /usr/local/bin/qemu"$QM_VER"-system-x86_64 /usr/local/bin/qemu-system-x86_64
 make clean
 
 ### arm
 ./configure --target-list=arm-softmmu && make -j
-mv arm-softmmu/qemu-system-arm /usr/local/bin/qemu"$QM_VER"-system-arm
+mv build/qemu-system-arm /usr/local/bin/qemu"$QM_VER"-system-arm
 ln -s /usr/local/bin/qemu"$QM_VER"-system-arm /usr/local/bin/qemu-system-arm
 make clean
 
 ### aarch64
 ./configure --target-list=aarch64-softmmu && make -j
-mv aarch64-softmmu/qemu-system-aarch64 /usr/local/bin/qemu"$QM_VER"-system-aarch64
+mv build/qemu-system-aarch64 /usr/local/bin/qemu"$QM_VER"-system-aarch64
 ln -s /usr/local/bin/qemu"$QM_VER"-system-aarch64 /usr/local/bin/qemu-system-aarch64
 make clean
 
 ### riscv32
-./configure --target-list=risv32-softmmu && make -j
-mv riscv32-softmmu/qemu-system-risv32 /usr/local/bin/qemu"$QM_VER"-system-riscv32
-ln -s /usr/local/bin/qemu"$QM_VER"-system-risv32 /usr/local/bin/qemu-system-risv32
+./configure --target-list=riscv32-softmmu && make -j
+mv build/qemu-system-riscv32 /usr/local/bin/qemu"$QM_VER"-system-riscv32
+ln -s /usr/local/bin/qemu"$QM_VER"-system-riscv32 /usr/local/bin/qemu-system-riscv32
 make clean
 
 ### riscv64
-./configure --target-list=risv64-softmmu && make -j
-mv riscv64-softmmu/qemu-system-arm /usr/local/bin/qemu"$QM_VER"-system-riscv64
-ln -s /usr/local/bin/qemu"$QM_VER"-system-risv64 /usr/local/bin/qemu-system-risv64
+./configure --target-list=riscv64-softmmu && make -j
+mv build/qemu-system-riscv64 /usr/local/bin/qemu"$QM_VER"-system-riscv64
+ln -s /usr/local/bin/qemu"$QM_VER"-system-riscv64 /usr/local/bin/qemu-system-riscv64
 make clean
 
 unset QEMU_VERSION
 unset QM_VER
+
+cd /exports
+## Klee
+git clone https://github.com/klee/klee.git
+cd klee
+mkdir -p ./build
+cd ./build
+### <asm/XXX.h>系のファイルが無い場合の対策.
+pushd /usr/include
+ln -s asm-generic asm
+popd
+cmake -DLLVMCC=/usr/bin/clang -DLLVMCXX=/usr/bin/clang++ -DCMAKE_BUILD_TYPE=Release  ..
+
+make check && make install 
+
+
+
+## Linux kernel source code
+cd /exports
+git clone --depth=1 git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+wget -O /usr/local/bin/extract-vmlinux https://raw.githubusercontent.com/torvalds/linux/master/scripts/extract-vmlinux
+chmod +x /usr/local/bin/extract-vmlinux
+cd linux/
+make mrproper
 
 cd /exports
 git clone ctftools:1u991yu24k1/my_ctf_tools.git 
@@ -146,23 +221,5 @@ cp -r vimrc ~/.vimrc
 cp -r bashrc ~/.bashrc
 cp -r ctf.sh /usr/local/bin/ctf.sh && chmod +x /usr/local/bin/ctf.sh
 
-cd ../
-
-
-## Linux kernel source code
-git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
-wget -O /usr/local/bin/extract-vmlinux https://raw.githubusercontent.com/torvalds/linux/master/scripts/extract-vmlinux
-chmod +x /usr/local/bin/extract-vmlinux
-cd linux/
-make mrproper
-
-#  Stop and disable unused service
-## cups, cups-browsed
-systemctl stop cups && systemctl disable cups
-systemctl stop cups-browsed && systemctl disable cups-browsed
-## default apache2
-systemctl stop apache2 && systemctl disable apache2
-
-## avahi
-systemctl mask avahi-daemon && systemctl stop avahi-daemon && systemctl disable avahi-daemon
-systemctl stop avahi-daemon.socket && systemctl disable avahi-daemon.socket
+cd /exports
+git clone https://github.com/bata24/gef.git
