616fe78a0cf7f4007cd89954eb6622505403db99
1u991yu24k1 <mycd9427@gmail.com>
2025-08-24 16:17:04 +0900

    fix x.py

--- x.py
+++ x.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 #-*-coding:utf-8-*-
-
+import sys
 from ptrlib import *
 
 def dbg(ss):
+  cs, ce = '\x1b[93;41m', '\x1b[0m' # white
   fmt = f"{ss}:"
   try:
     val = eval(ss.encode())
@@ -13,7 +14,7 @@ def dbg(ss):
       fmt += f" {val}"
   except NameError:
     fmt = fmt.rstrip(":")
-  logging.debug(cs + fmt + ce)
+  print(cs + fmt + ce)
 
 ##### addrs/offsets of symbols, gadgets and consts #####
 is_remote = len(sys.argv) > 1 and sys.argv[1] == 'r'
