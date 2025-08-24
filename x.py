e34d74821f02c37962dbd83c0545671561bdfda0
1u991yu24k1 <mycd9427@gmail.com>
2025-08-24 09:18:55 +0900

    remove ror/rol from x.py (use ptrlib-impl)

--- x.py
+++ x.py
@@ -15,41 +15,6 @@ def dbg(ss):
     fmt = fmt.rstrip(":")
   logging.debug(cs + fmt + ce)
 
-  
-def pQ(a): return struct.pack('<Q', a&0xffffffffffffffff)
-def p(a): return struct.pack('<I', a&0xffffffff)
-def uQ(a): return struct.unpack('<Q', a.ljust(8, b'\x00'))[0]
-def u(a): return struct.unpack('<I', a.ljust(4, b'\x00'))[0]
-
-def rol(x, rotate, bitwidth=64):
-  assert rotate < bitwidth
-  """
-  rotate left in bitwidth 
-  ex. 
-    0x11223344 -> 0x22334411 (8bit rotate left @32bit)
-    0x1122334455667788 -> 0x3344556677881122 (16bit rotate left @64bit) 
-  """
-  word_mask = (1 << bitwidth) - 1
-  right_mask = (1 << (bitwidth - rotate)) - 1
-  left = x&(word_mask - right_mask)
-  left >>= (bitwidth - rotate)
-  return ((x << rotate)&word_mask) | left
-
-def ror(x, rotate, bitwidth=64):
-  assert rotate < bitwidth
-  """
-  rotate right in bitwidth 
-  0x11223344 -> 0x44112233 (8bit rotate right @32bit)
-  0x1122334455667788 -> 0x7788112233445566 (16bit rotate right @64bit)
-  """
-  word_mask = (1 << bitwidth) - 1
-  right_mask = (1 << rotate) - 1
-  right = (x&right_mask)
-  shifter = bitwidth - rotate
-  return (right << shifter) | ((x >> rotate)&word_mask)
-
-   
-
 ##### addrs/offsets of symbols, gadgets and consts #####
 is_remote = len(sys.argv) > 1 and sys.argv[1] == 'r'
 
